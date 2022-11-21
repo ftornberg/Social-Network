c597480c32c21d8e6bdfd309ebd5cfbd10c8a051
Max Thörnblad <max.thornblad@hotmail.com>
2022-11-21 16:24:09 +0100

    Changed classes

--- Entity/Conversation.cs
+++ Entity/Conversation.cs
@@ -3,9 +3,5 @@ public class Conversation
 {
     public Guid ConversationId { get; set; }
 
-    public Guid Sender { get; set; }
-
-    public Guid Reciever { get; set; }
-
     public List<DirectMessage>? Messages { get; set; }
 }
