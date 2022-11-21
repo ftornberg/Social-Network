c8a01fd39b3cd8617632c1cec7c80eb282319362
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-11-21 16:06:00 +0100

    Added class for post and comments

---
+++ Entity/Post.cs
@@ -0,0 +1,24 @@
+﻿namespace Entity;
+public partial class Post
+{
+  public Guid PostId { get; set; }
+
+  public Guid PostUserId { get; set; }
+
+  public string? PostedMessage { get; set; }
+
+  public DateTime PostedTime { get; set; }
+
+  public static Post CreatePost(Guid postUserId, string? postedMessage, DateTime postedAt)
+  {
+    return new Post
+    {
+      PostId = Guid.NewGuid(),
+      PostUserId = postUserId,
+      PostedMessage = postedMessage,
+      PostedTime = postedAt
+    };
+  }
+
+
+}
