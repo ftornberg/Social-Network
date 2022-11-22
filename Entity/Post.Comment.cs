c8c378d44467e30360a1b82767d1462e53a6ac58
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-11-22 16:41:11 +0100

    Skapade ett testprojekt, dependent till Entity. Refaktorerade Post och Post.Comment

--- Entity/Post.Comment.cs
+++ Entity/Post.Comment.cs
@@ -3,22 +3,14 @@ public partial class Post
 {
   public class Comment : Post
   {
-    public Guid CommentId { get; set; }
+    public new int Id { get; set; }
 
-    public Guid CommentUserId { get; set; }
+    public Post? PostId { get; set; }
 
-    public string? CommentMessage { get; set; }
+    public string? Message { get; set; }
 
     public DateTime CommentedTime { get; set; }
-  }
-  public static Comment CreateComment(Guid commentUserId, string? commentMessage, DateTime commentedAt)
-  {
-    return new Comment
-    {
-      CommentId = Guid.NewGuid(),
-      CommentUserId = commentUserId,
-      CommentMessage = commentMessage,
-      CommentedTime = commentedAt
-    };
+    
+    public int UserId { get; set; }
   }
 }
--- Entity/Post.cs
+++ Entity/Post.cs
@@ -1,24 +1,14 @@
 ﻿namespace Entity;
 public partial class Post
 {
-  public Guid PostId { get; set; }
+    public int Id { get; set; }
+    
+    public int UserId { get; set; }
 
-  public Guid PostUserId { get; set; }
+    public string? PostedMessage { get; set; }
 
-  public string? PostedMessage { get; set; }
+    public List<Comment>? Comments { get; set; }
 
-  public DateTime PostedTime { get; set; }
-
-  public static Post CreatePost(Guid postUserId, string? postedMessage, DateTime postedAt)
-  {
-    return new Post
-    {
-      PostId = Guid.NewGuid(),
-      PostUserId = postUserId,
-      PostedMessage = postedMessage,
-      PostedTime = postedAt
-    };
-  }
-
-
+    public DateTime PostedTime { get; set; }
+    
 }
