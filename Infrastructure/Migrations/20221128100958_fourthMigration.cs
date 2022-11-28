4aafd0deaa6494cba217c056594e4044c09a6cda
Fredrik Törnberg <fredrik.tornberg.syne21lin@tucsweden.se>
2022-11-28 11:44:52 +0100

    Added migrations

---
+++ Infrastructure/Migrations/20221128100958_fourthMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fourthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2022, 11, 28, 11, 9, 57, 850, DateTimeKind.Local).AddTicks(9692));
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedTime", "Email", "Name", "Password", "UserId" },
+                values: new object[] { 2, new DateTime(2022, 11, 28, 11, 9, 57, 850, DateTimeKind.Local).AddTicks(9817), "bill@email.com", "Bill", "password", 2 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2022, 11, 25, 14, 14, 35, 497, DateTimeKind.Local).AddTicks(8278));
+        }
+    }
+}
