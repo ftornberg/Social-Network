3e26663ca456db6e2bed2f20a913e85155034a83
Max Thörnblad <max.thornblad@hotmail.com>
2022-11-30 14:36:15 +0100

    Implemented typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ShowUsers from './component/ShowUsers';
 import ShowUser from './component/ShowUser';
@@ -8,7 +7,6 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
 				Show Users
 				<ShowUsers />
 				Show User
