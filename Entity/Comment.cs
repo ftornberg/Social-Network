65146ec95c6ddf824790a28a47f3f4fb8f8275f6
Tobias Parnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2022-12-03 16:11:47 +0100

    Comment

--- Entity/Comment.cs
+++ Entity/Comment.cs
@@ -1,9 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Entity;
 public class Comment : BaseEntity
 {
+    public Comment()
+    {
+    }
+
+    public Comment(int postId, int commentedByUserId, string message, DateTime commentedTime)
+    {
+      PostId = postId;
+      CommentedByUserId = commentedByUserId;
+      Message = message;
+      CommentedTime = commentedTime;
+    }
+
+  [ForeignKey("PostId")]
   public int PostId { get; set; }
 
-  public User PostedBy { get; set; }
+  public int CommentedByUserId { get; set; }
 
   public string? Message { get; set; }
 
