3a258dcba3a6ce307432be20af1ed5e80e027356
ISAC\isacg <isac@webking.se>
2022-12-08 15:22:45 +0100

    Implemented Name for Follower

--- API/Dto/Follower/FollowerDto.cs
+++ API/Dto/Follower/FollowerDto.cs
@@ -3,7 +3,12 @@ namespace API.Dto.Follower
     public class FollowerDto
     {
         public int FollowerUserId { get; set; }
+
+        public string FollowerUserName { get; set; }
         
         public int FollowsUserId { get; set; }
+
+        public string FollowsUserName { get; set; }
+
     }
 }
