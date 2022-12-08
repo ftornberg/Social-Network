43a8bb30df4f1795048bc5a9f0c3d14906d812d8
ISAC\isacg <isac@webking.se>
2022-12-08 18:24:44 +0100

    GlobalTestUsings

---
+++ Social-Network.Test/GlobalTestUsings.cs
@@ -0,0 +1,7 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using API.Controllers;
+global using API.Helpers;
+global using AutoMapper;
+global using Entity;
+global using Entity.Interfaces;
+global using Moq;
--- Social-Network.Test/UserTests/PostTest.cs
+++ Social-Network.Test/UserTests/PostTest.cs
@@ -1,43 +1,35 @@
-﻿using AutoMapper;
-using Entity.Interfaces;
-using Entity;
-using Moq;
-using API.Helpers;
-using API.Controllers;
-using API.Dto;
+﻿namespace Social_Network.Test.UserTests;
 
-namespace Social_Network.Test.UserTests
+[TestClass]
+public class PostControllerTest
 {
-    [TestClass]
-    public class PostControllerTest
+    private IMapper? _mapper;
+    private Mock<IGenericRepository<Post>>? _postRepositoryMock;
+    private Mock<IGenericRepository<Follower>>? _followerRepositoryMock;
+    private Mock<IGenericRepository<User>>? _userRepositoryMock;
+
+    [TestInitialize]
+    public void Setup()
     {
-        private IMapper? _mapper;
-        private Mock<IGenericRepository<Post>>? _postRepositoryMock;
-        private Mock<IGenericRepository<Follower>>? _followerRepositoryMock;
-        private Mock<IGenericRepository<User>>? _userRepositoryMock;
-
-        [TestInitialize]
-        public void Setup()
+        var mappingConfig = new MapperConfiguration(mc =>
         {
-            var mappingConfig = new MapperConfiguration(mc =>
+            mc.AddProfile(new MappingProfile());
+        });
+        var mapper = mappingConfig.CreateMapper();
+        _mapper = mapper;
+        _postRepositoryMock = new Mock<IGenericRepository<Post>>();
+        _followerRepositoryMock = new Mock<IGenericRepository<Follower>>();
+        _userRepositoryMock = new Mock<IGenericRepository<User>>();
+    }
+
+    [TestMethod]
+    public async Task TestShouldGetPostsPostedToSpecificUserFromControllerAsync()
+    {
+        var userIdToTest = 2;
+
+        _postRepositoryMock?.Setup(x => x.ListAllAsync())
+            .ReturnsAsync(new List<Post>
             {
-                mc.AddProfile(new MappingProfile());
-            });
-            var mapper = mappingConfig.CreateMapper();
-            _mapper = mapper;
-            _postRepositoryMock = new Mock<IGenericRepository<Post>>();
-            _followerRepositoryMock = new Mock<IGenericRepository<Follower>>();
-            _userRepositoryMock = new Mock<IGenericRepository<User>>();
-        }
-
-        [TestMethod]
-        public async Task TestShouldGetPostsPostedToSpecificUserFromControllerAsync()
-        {
-            var userIdToTest = 2;
-
-            _postRepositoryMock?.Setup(x => x.ListAllAsync())
-                .ReturnsAsync(new List<Post>
-                {
                     new Post{
                         Id = 1,
                         PostedMessage = "Hej",
@@ -62,43 +54,43 @@ namespace Social_Network.Test.UserTests
                         PostedToUserId = userIdToTest,
                         PostedTime = DateTime.Now
                     }
-                });
+            });
 
-            // Act 1
-            var postController = new PostController(_postRepositoryMock.Object, _mapper, _followerRepositoryMock.Object, _userRepositoryMock.Object);
-            var postDto = await postController.GetPostsToSpecificUserAsync(userIdToTest);
+        // Act 1
+        var postController = new PostController(_postRepositoryMock.Object, _mapper, _followerRepositoryMock.Object, _userRepositoryMock.Object);
+        var postDto = await postController.GetPostsToSpecificUserAsync(userIdToTest);
 
-            var containsOnlyCorrectUserId = true;
-            foreach (var item in postDto.Value)
-            {
-                if(item.PostedToUserId != userIdToTest)
-                    containsOnlyCorrectUserId = false;
-            }
-            
-            // Assert 1
-            Assert.AreEqual("Hello2", postDto.Value[0].PostedMessage);
-            Assert.AreNotEqual("Hello", postDto.Value[1].PostedMessage);
-            Assert.AreEqual("Hej", postDto.Value[1].PostedMessage);
-            Assert.AreEqual(2, postDto.Value.Count);
-            Assert.IsTrue(containsOnlyCorrectUserId);
+        var containsOnlyCorrectUserId = true;
+        foreach (var item in postDto.Value)
+        {
+            if (item.PostedToUserId != userIdToTest)
+                containsOnlyCorrectUserId = false;
         }
 
-        [TestMethod]
-        public async Task TestShouldGetAllPostsSortedByDescendingFromControllerAsync()
+        // Assert 1
+        Assert.AreEqual("Hello2", postDto.Value[0].PostedMessage);
+        Assert.AreNotEqual("Hello", postDto.Value[1].PostedMessage);
+        Assert.AreEqual("Hej", postDto.Value[1].PostedMessage);
+        Assert.AreEqual(2, postDto.Value.Count);
+        Assert.IsTrue(containsOnlyCorrectUserId);
+    }
+
+    [TestMethod]
+    public async Task TestShouldGetAllPostsSortedByDescendingFromControllerAsync()
+    {
+        _followerRepositoryMock?.Setup(x => x.ListAllAsync())
+        .ReturnsAsync(new List<Follower>
         {
-            _followerRepositoryMock?.Setup(x => x.ListAllAsync())
-            .ReturnsAsync(new List<Follower>
-            {
                 new Follower
                 (
                     2,
                     1
                 )
-            }
-            );
-            _postRepositoryMock?.Setup(x => x.ListAllAsync())
-                .ReturnsAsync(new List<Post>
-                {
+        }
+        );
+        _postRepositoryMock?.Setup(x => x.ListAllAsync())
+            .ReturnsAsync(new List<Post>
+            {
                     new Post{
                         Id = 1,
                         PostedMessage = "Hej",
@@ -127,19 +119,18 @@ namespace Social_Network.Test.UserTests
                         PostedToUserId = 2,
                         PostedTime = DateTime.Now
                     }
-                });
+            });
 
-            // Act 1
-            var postController = new PostController(_postRepositoryMock.Object, _mapper, _followerRepositoryMock.Object, _userRepositoryMock.Object);
-            var postDto = await postController.GetAllPostsAsync(2);
-            
-            // Assert 1
-            Assert.AreEqual(3, postDto.Value.Count);
-            
-            Assert.AreEqual("Hello2", postDto.Value[0].PostedMessage);
-            Assert.AreEqual("Hello", postDto.Value[1].PostedMessage);
-            Assert.AreEqual("Hej", postDto.Value[2].PostedMessage);
+        // Act 1
+        var postController = new PostController(_postRepositoryMock.Object, _mapper, _followerRepositoryMock.Object, _userRepositoryMock.Object);
+        var postDto = await postController.GetAllPostsAsync(2);
 
-        }
+        // Assert 1
+        Assert.AreEqual(3, postDto.Value.Count);
+
+        Assert.AreEqual("Hello2", postDto.Value[0].PostedMessage);
+        Assert.AreEqual("Hello", postDto.Value[1].PostedMessage);
+        Assert.AreEqual("Hej", postDto.Value[2].PostedMessage);
+
     }
 }
